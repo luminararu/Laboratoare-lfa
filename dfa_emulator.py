bb27dc5654875d423678a7fecc946afa52d7dd4a
luminararu <81523632+luminararu@users.noreply.github.com>
2024-07-01 17:41:51 +0300

    Update dfa_emulator.py

--- dfa_emulator.py
+++ dfa_emulator.py
@@ -4,6 +4,8 @@ parsat=parser.load_file('inputfile')
 
 states=[]
 sf=[]
+
+# separam starile si le introducem in vectorul states. De asemenea, extragem starea de inceput(in start) si cea de final in sf
 for stari in parsat['States']:
     if 'S' in stari :
         stari = stari.split(', ')
@@ -21,6 +23,8 @@ s=set(parsat['Sigma'])
 
 
 d={}
+
+# folosim un dictionar pentru a retine tranzitiile ( functia delta )
 for tranz in parsat['Transitions']:
     elem = tranz.strip().split(', ')
     print(elem)
@@ -32,10 +36,12 @@ for tranz in parsat['Transitions']:
 
 str=input()
 current=start
+# citim string ul si retinem starea curenta in current iar pentru ficare litera aplicam functia delta
 for i in range(0, len(str)):
     if str[i] in s:
         current=d[current][str[i]]
 
+# verificam daca ultima stare se afla in sf( final states)
 if current in sf:
     print("acceptat")
 else:
