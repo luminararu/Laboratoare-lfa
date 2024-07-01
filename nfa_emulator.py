22f41de8aa0fc5a0c888ca90026b56aeb340f8ca
luminararu <81523632+luminararu@users.noreply.github.com>
2024-07-01 17:59:18 +0300

    Update nfa_emulator.py

--- nfa_emulator.py
+++ nfa_emulator.py
@@ -4,6 +4,7 @@ parsat=parser.load_file('inputfile')
 
 states=[]
 sf=[]
+# se extrag starile si se retine starea de inceput si starea de final analizandu-se  cazurle in care pe o linie avem S, F sau S si F( starea de inceput este si stare de final)
 for stare in parsat['States']:
     if 'S' in stare and 'F' in stare:
         stare = stare.split(', ')
@@ -30,6 +31,7 @@ s=set(parsat['Sigma'])
 
 d={}
 
+# se retin tranzitiile intr-un dicionar care va definii functia delta 
 for tranz in parsat['Transitions']:
     elem = tranz.strip().split(', ')
     print(elem)
@@ -41,11 +43,12 @@ for tranz in parsat['Transitions']:
     d[elem[0]][elem[1]].append(elem[2])
 
 st=input()
+# parc_curent va retine elemente care sunt parcurse simulta la momentul citirii
 parc_curent=[]
 index=0
 parc_curent.append(start)
 
-
+# se parcurge string ul si se aplica funtia delta pentru fiecare stare din parcurgerea curenta creandu-se astfel un nou vector parc_new care va fi nou parc_curent
 for i in range(0, len(st)):
     parc_new=[]
     for star in parc_curent:
